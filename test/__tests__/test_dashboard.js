1f9c849bfc1af7ea16ad1fe34d552ed5ced4a961
Emre Surmeli <s.emre.s.8@gmail.com>
2015-06-19 08:32:35 -0700

    gunna make some funny tests

--- test/__tests__/test_dashboard.js
+++
@@ -1,9 +0,0 @@
-var React     = require('react/addons');
-var TestUtils = React.addons.TestUtils;
-
-describe('dashboard', function() {
-  it('should be composed of msgs and the msg thread', function() {
-
-    expect(true).toEqual(true);
-  });
-});
--- test/__tests__/user_create_test.js
+++
@@ -1,9 +0,0 @@
-var React = require('react/addons');
-var TestUtils = React.addons.TestUtils;
-var expect = require('expect');
-
-describe('user creation', function() {
-  it('should create a user, yo', function() {
-    expect(true).toEqual(true);
-  });
-});
